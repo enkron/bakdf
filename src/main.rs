458ba6b879dd42f6652f4bb8f6702c94ec6e7b61
Sergei Belokon <srj.belokon@gmail.com>
2021-04-14 00:12:31 +0300

    OBS-01 Made constructor for the `Config` struct

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 extern crate serde;
 
 use serde::Deserialize;
-use std::{env, error, fs};
+use std::{env, error::Error, fs};
 use toml;
 
-fn main() -> Result<(), Box<dyn error::Error + 'static>> {
+fn main() -> Result<(), Box<dyn Error + 'static>> {
     // read a list of params
     let args: Vec<String> = env::args().collect();
 
@@ -13,19 +13,27 @@ fn main() -> Result<(), Box<dyn error::Error + 'static>> {
     // - Pull out all to separate functions, structures etc
     // - Move all stuff to lib.rs
 
-    // read configuration file into a string
-    let config_str = fs::read_to_string(&args[1])?;
-    // get .toml structure from string
-    let config: Config = toml::from_str(&config_str)?;
-
     // debug
-    println!("{:?}", config.dotfiles);
+    println!("{:?}", Config::new(&args));
 
     Ok(()) // return the `unit` struct
 }
 
-// bring the Deserialize trait which transforms string into the struct
+// bring the Deserialize trait which transforms string into a struct
 #[derive(Deserialize, Debug)]
 struct Config {
     dotfiles: Vec<String>,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, Box<dyn Error + 'static>> {
+        // read configuration file into a string
+        let config_str = fs::read_to_string(&args[1])?;
+        // get .toml structure from string
+        let config: Config = toml::from_str(&config_str)?;
+
+        let dotfiles = config.dotfiles;
+
+        Ok(Config { dotfiles })
+    }
 }
