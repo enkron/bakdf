ccc1822119135b90d54cdae6b009301b29c74b21
Sergei Belokon <srj.belokon@gmail.com>
2021-04-14 00:47:41 +0300

    OBS-01 Removed `static from Box<dyn Error + `static>
    Need to investigate: why + `static lifetime parameter is needed in the
    func signature
    
    For starting point rfcs#2504 could help
    https://rust-lang.github.io/rfcs/2504-fix-error.html#why-cause---source

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use serde::Deserialize;
 use std::{env, error::Error, fs};
 use toml;
 
-fn main() -> Result<(), Box<dyn Error + 'static>> {
+fn main() -> Result<(), Box<dyn Error>> {
     // read a list of params
     let args: Vec<String> = env::args().collect();
 
@@ -12,11 +12,11 @@ fn main() -> Result<(), Box<dyn Error + 'static>> {
     // - Add error handling
     // - Pull out all to separate functions, structures etc
     // - Move all stuff to lib.rs
+    // - Implement tests
 
-    // debug
-    println!("{:?}", Config::new(&args));
+    println!("{:?}", Config::new(&args)); // DEBUG
 
-    Ok(()) // return the `unit` struct
+    Ok(())
 }
 
 // bring the Deserialize trait which transforms string into a struct
@@ -26,7 +26,7 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Result<Config, Box<dyn Error + 'static>> {
+    fn new(args: &[String]) -> Result<Config, Box<dyn Error>> {
         // read configuration file into a string
         let config_str = fs::read_to_string(&args[1])?;
         // get .toml structure from string
