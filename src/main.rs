ab31fb7b4605f093c6032b67c2673495e7502ae4
Sergei Belokon <srj.belokon@gmail.com>
2021-04-16 00:46:19 +0300

    OBS-03 Made config.toml file independent from particular argument index
    But if was dirty hack with initializing of mut emty Vec
    and then re-assign args string to it

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 extern crate serde;
 
 use serde::Deserialize;
-use std::{env, error::Error, fs};
+use std::{env, error, fs};
 use toml;
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> Result<(), Box<dyn error::Error>> {
     // read a list of params
-    let args: Vec<String> = env::args().collect();
+    let args: Vec<String> = env::args().skip(1).collect();
 
     // TODO:
     // - Add error handling
@@ -15,19 +15,22 @@ fn main() -> Result<(), Box<dyn Error>> {
     // - Implement tests
     // - Unbind args from hardcoded args[1] position
 
-    println!("{:?}", Config::new(&args)); // DEBUG
+    println!("{:?}", Config::new(&args)?); // DEBUG
 
     Ok(())
 }
 
-// bring the Deserialize trait which transforms string into a struct
+// bring the Deserialize trait
+// which transforms string into a struct
 #[derive(Deserialize, Debug)]
 struct Config {
     dotfiles: Vec<String>,
 }
 
 impl Config {
-    fn new(args: &[String]) -> Result<Config, Box<dyn Error>> {
+    fn new(args: &[String]) -> Result<Config, Box<dyn error::Error>> {
+        let mut dotfiles = vec![];
+
         for arg in args.iter() {
             if arg == "config.toml" {
                 // read configuration file into a string
@@ -36,7 +39,9 @@ impl Config {
                 // get .toml structure from string
                 let config: Config = toml::from_str(&config_str)?;
 
-                let dotfiles = config.dotfiles;
+                dotfiles = config.dotfiles; // shadows previous empty `dotfiles` var
+            } else {
+                return Err(Box::from("config.toml was not provided"));
             }
         }
 
