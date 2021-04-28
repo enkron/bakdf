4cd8ca779327531f134dde97334ad3021f6625cf
Sergei Belokon <srj.belokon@gmail.com>
2021-04-29 01:03:19 +0300

    OBS-16 Added `clap` crate
    Also added simple example into the main.rs file (commented lines)
    Actually using clap library breaks existing argument parsing.
    
    Need to fugure out how to combine existing approach with the clap library

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+//extern crate clap;
 extern crate serde;
 
+//use clap::{App, Arg};
 use std::{env, error, process};
 use tmp::Config;
 
@@ -17,6 +19,13 @@ fn main() -> Result<(), Box<dyn error::Error>> {
         eprintln!("error: Cannot read target field in the {}", CONFIG);
         process::exit(1);
     };
+
+    //    let matches = App::new("tmp")
+    //        .author(clap::crate_authors!("\n"))
+    //        .arg(Arg::with_name("config.toml").last(true))
+    //        .get_matches();
+    //
+    //    println!("{:?}", matches);
 
     Ok(())
 }
